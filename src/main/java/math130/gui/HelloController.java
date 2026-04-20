74992f8f089c9ce3cbc43bc9a99a3b55ea201693
Yugioh <cg1186682@swccd.edu>
2026-04-20 00:31:22 -0700

    Wrote Yu Gi Oh!

--- src/main/java/math130/gui/HelloController.java
+++ src/main/java/math130/gui/HelloController.java
@@ -3,12 +3,20 @@ package math130.gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+    //@FXML
+    //private ImageView blueEyes;
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+       // blueEyes.setImage(new image(getClass().getResourceAsStream("resources/math130.gui/SacredFireKingGarunix.jpg")));
     }
+
+    //@FXML
+    //private ImageView redEyes;
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module math130.gui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens math130.gui to javafx.fxml;
